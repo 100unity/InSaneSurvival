bdc5d60e112974b70d896152c3a95e36261f8969
Samuel Günther <samuell.guenther@gmail.com>
2019-11-15 17:27:36 +0100

    implemented basic enemy movement

---
+++ Assets/Scripts/PlayerManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerManager : MonoBehaviour
+{
+    #region Singleton
+    private static PlayerManager _instance;
+    public static PlayerManager Instance { get { return _instance; } }
+
+    private void Awake()
+    {
+        if (_instance != null && _instance != this)
+        {
+            Destroy(this.gameObject);
+        } else
+        {
+            _instance = this;
+        }
+    }
+    #endregion
+
+    [SerializeField] private GameObject player;
+
+    public void SetPlayer(GameObject player)
+    {
+        this.player = player;
+    }
+
+    public GameObject GetPlayer()
+    {
+        return player;
+    }
+
+}
