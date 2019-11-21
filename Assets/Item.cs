398319a682503cf013e54086d5234b5f7b336f9e
thuemmels <thummel.stephan@yahoo.de>
2019-11-21 13:00:45 +0100

    Adds basic inventory system with Items.

---
+++ Assets/Item.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Item", menuName = "Inventory/Item")]
+public class Item : ScriptableObject
+{
+    [SerializeField]
+    new private string name = "New Item";
+    [SerializeField]
+    private Sprite icon = null;
+}
