4a26030c3bb4f921cf2b88a83cd9269e7c93a190
cuongtm95 <45611820+cuongtm95@users.noreply.github.com>
2019-11-30 16:36:23 +0100

    Item Pickup done

---
+++ Assets/Scripts/Interactables/Crate.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Crate : Interactable
+{
+    Animator animator;
+
+    [SerializeField] bool isOpen;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    public override void Interact()
+    {
+        base.Interact();
+        if (!isOpen)
+        {
+            isOpen = true;
+            animator.SetBool("open", isOpen);
+        }
+    }
+}
---
+++ Assets/Scripts/Interactables/ItemPickup.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemPickup : Interactable
+{
+    public Item item;
+
+    public Inventory inventory;
+
+    public override void Interact()
+    {
+        base.Interact();
+        PickUp();
+    }
+
+    void PickUp()
+    {
+        //Pick up item
+        inventory.Add(item);
+        //Add to inventory
+        Destroy(gameObject);
+    }
+}
