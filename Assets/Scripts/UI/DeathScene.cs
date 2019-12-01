d02ee5011bafaaa997929e3eae42e8a65918cf05
Tobi Hoffmann <tobi.hoffmann@protonmail.com>
2019-12-01 13:34:03 +0100

    added death animation

---
+++ Assets/Scripts/UI/DeathScene.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UI;
+using UnityEngine;
+
+public class DeathScene : MonoBehaviour
+{
+    [SerializeField] private FadeElement whiteLight;
+    [SerializeField] private GameObject playerCharacter;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        whiteLight.FadeOut(3f);
+        playerCharacter.GetComponent<Animator>().SetBool ("isDead", true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
