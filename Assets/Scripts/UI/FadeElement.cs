0d1a8ca5e212f69adb9a18d7aec4425fbbeccd50
Tobi Hoffmann <tobi.hoffmann@protonmail.com>
2019-12-07 16:32:28 +0100

    Added files from old branch

---
+++ Assets/Scripts/UI/FadeElement.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+namespace UI
+{
+    public class FadeElement : MonoBehaviour
+    {
+        private Image _element;
+
+        private void Awake()
+        {
+            _element = gameObject.GetComponent<Image>();
+        }
+
+        public void FadeIn(float duration)
+        {
+            StartCoroutine(FadeInCr(duration));
+        }
+    
+        private IEnumerator FadeInCr(float duration)
+        {
+            float currentTime = 0f;
+
+            while (currentTime < duration)
+            {
+                float alpha = Mathf.Lerp(0f, 1f, currentTime / duration);
+                _element.color = new Color(_element.color.r, _element.color.g, _element.color.b, alpha);
+                currentTime += Time.deltaTime;
+                yield return null;
+            }
+        }
+        
+        public void FadeOut(float duration)
+        {
+            StartCoroutine(FadeOutCr(duration));
+        }
+    
+        private IEnumerator FadeOutCr(float duration)
+        {
+            float currentTime = 0f;
+
+            while (currentTime < duration)
+            {
+                float alpha = Mathf.Lerp(1f, 0f, currentTime / duration);
+                _element.color = new Color(_element.color.r, _element.color.g, _element.color.b, alpha);
+                currentTime += Time.deltaTime;
+                yield return null;
+            }
+            gameObject.SetActive(false);
+        }
+    }
+
+}
---
+++ Assets/Scripts/UI/Menus/DeathMenu.cs
@@ -0,0 +1,44 @@
+﻿using Managers;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+namespace UI.Menus
+{
+    public class DeathMenu : MonoBehaviour
+    {
+        [Tooltip("The content of the death menu")] [SerializeField]
+        private GameObject deathContent;
+        
+        [Tooltip("The button for going back to the main menu")] [SerializeField]
+        private Button mainMenuButton;
+
+        /// <summary>
+        /// Adds all listeners
+        /// </summary>
+        private void Awake()
+        {
+            mainMenuButton.onClick.AddListener(GoToMainMenu);
+        }
+        
+        /// <summary>
+        /// Loads the MainMenuScene
+        /// </summary>
+        private void GoToMainMenu()
+        {
+            SceneManager.LoadScene(Consts.Scene.MAIN_MENU);
+            SceneManager.sceneLoaded += SceneLoadCompleted;
+        }
+
+        /// <summary>
+        /// This will wait until the MainMenuScene is loaded before hiding the PauseMenu
+        /// </summary>
+        /// <param name="scene">The loaded scene</param>
+        /// <param name="mode">The loading mode</param>
+        private void SceneLoadCompleted(Scene scene, LoadSceneMode mode)
+        {
+            if (scene.buildIndex != Consts.Scene.MAIN_MENU) return;
+            SceneManager.sceneLoaded -= SceneLoadCompleted;
+        }
+    }
+}
