0bd22759942040c513a7d40ee9281cceaaf6ba74
Tobi Hoffmann <tobi.hoffmann@protonmail.com>
2019-12-07 17:56:16 +0100

    integrated fade method into DeathScene.cs to rm FadeElements

--- Assets/Scripts/UI/DeathScene.cs
+++ Assets/Scripts/UI/DeathScene.cs
@@ -1,23 +1,41 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using Constants;
 using UI;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Serialization;
+using UnityEngine.UI;
 
 public class DeathScene : MonoBehaviour
 {
-    [SerializeField] private FadeElement whiteLight;
+    [SerializeField] private Image whitePanel;
     [SerializeField] private GameObject playerCharacter;
 
     // Start is called before the first frame update
+
     void Start()
     {
-        whiteLight.FadeOut(2.5f);
+        StartCoroutine( FadeOut(2.5f));
         playerCharacter.GetComponent<Animator>().SetBool ("isDead", true);
     }
 
-    // Update is called once per frame
-    void Update()
+    // fades out white panel
+    private IEnumerator FadeOut(float fadeDuration)
     {
+        float currentTime = 0f;
+        Color color = whitePanel.color;
+            
+        while (currentTime < fadeDuration)
+        {
+            float alpha = Mathf.Lerp(1f, 0f, currentTime / fadeDuration);
+            color = new Color(color.r, color.g, color.b, alpha);
+            whitePanel.color = color;
+            currentTime += Time.deltaTime;
+            yield return null;
+        }
+        yield return new WaitForSeconds(fadeDuration);
         
     }
 }
