24223c663607fddbfbe0cc6c8e54859da54c968b
inkompotato <fra.js.1998@gmail.com>
2019-12-12 15:20:51 +0100

    first thoughts

---
+++ Assets/Scripts/AbstractClasses/ISaveable.cs
@@ -0,0 +1,13 @@
+﻿using Utils;
+
+namespace Interfaces
+{
+    public interface ISaveable
+    {
+        // object is able to convert its current state to JSON
+        Save SaveToJson();
+        
+        // object can set its own state based on data from the JSON file
+        void LoadFromJson(Save input);
+    }
+}
