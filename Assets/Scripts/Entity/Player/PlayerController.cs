5c42c2bc2e2cb3b72bb625a1fcc16c8703c5d9d0
Samuel Günther <samuell.guenther@gmail.com>
2019-12-14 01:13:55 +0100

    fix

--- Assets/Scripts/Entity/Player/PlayerController.cs
+++ Assets/Scripts/Entity/Player/PlayerController.cs
@@ -151,7 +151,6 @@ namespace Entity.Player
 
                 if (objectHit.GetComponent<Damageable>() is Damageable damageable)
                 {
-                    Debug.Log("ATTACK");
                     _interactLogic.RemoveFocus();
                     // implementation NOT capable of area damage
                     _attackLogic.StartAttack(objectHit);
