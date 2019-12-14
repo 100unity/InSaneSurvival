73bab4725938b15556813dbdbbc23f495766bdfb
David <panagiotopulos.d@gmail.com>
2019-12-14 01:24:26 +0100

    [Started with Equipable "Abilities"]

--- Assets/Scripts/Inventory/Equipable.cs
+++ Assets/Scripts/Inventory/Equipable.cs
@@ -1,21 +1,54 @@
-﻿using System.Collections;
+﻿using System;
 using System.Collections.Generic;
 using UnityEngine;
-using Inventory;
+using Utils;
 
-[CreateAssetMenu(fileName = "New Equipable", menuName = "Inventory/Item/Equipable")]
-public class Equipable : Item
+namespace Inventory
 {
-    /// <summary>
-    /// Placeholder function for now.
-    /// </summary>
-    public new bool Use()
+    [CreateAssetMenu(fileName = "New Equipable", menuName = "Inventory/Item/Equipable")]
+    public class Equipable : Item
     {
-        Debug.Log("Equipping the item " + name);
-        return false;
-    }
-    public override bool Equals(object other)
-    {
-        return other is Equipable item && item.name == name;
+        public enum EquipableAbility
+        {
+            Chop,
+            Mine
+        }
+
+        [EnumMultiSelect] [SerializeField] private EquipableAbility equipableAbility;
+
+
+        /// <summary>
+        /// Returns a list of all abilities
+        /// </summary>
+        public List<int> Abilities => GetSelectedElements();
+
+        /// <summary>
+        /// Placeholder function for now.
+        /// </summary>
+        public new bool Use()
+        {
+            Debug.Log("Equipping the item " + name);
+            return false;
+        }
+
+        public override bool Equals(object other)
+        {
+            return other is Equipable item && item.name == name;
+        }
+
+        private List<int> GetSelectedElements()
+        {
+            List<int> selectedElements = new List<int>();
+            for (int i = 0; i < Enum.GetValues(typeof(EquipableAbility)).Length; i++)
+            {
+                int layer = 1 << i;
+                if (((int) equipableAbility & layer) != 0)
+                {
+                    selectedElements.Add(i);
+                }
+            }
+
+            return selectedElements;
+        }
     }
 }
