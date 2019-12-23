c9e410d2c601f5c7d881a7fd68fcc943e240b792
Tobi Hoffmann <tobi.hoffmann@protonmail.com>
2019-12-23 18:24:36 +0100

    initial

---
+++ Assets/Scripts/GameTime/Clock.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using Managers;
+using UnityEngine;
+
+namespace GameTime
+{
+    public class Clock : MonoBehaviour
+    {
+        [SerializeField][Tooltip("Day length in minutes")]
+        private float dayLength;
+    
+        [SerializeField][Tooltip("Year length in days")]
+        private int yearLength;
+
+        public int YearLength
+        {
+            get { return yearLength; }
+        }
+
+        [SerializeField] [Tooltip("Current time")] [Range(0f, 1f)]
+        private float timeOfDay;
+        public float TimeOfDay
+        {
+            get { return timeOfDay; }
+        }
+
+        private int _days;
+        public int Days
+        {
+            get { return _days; }
+        }
+        
+        private int _years;
+
+        public int Years
+        {
+            get { return _years; }
+        }
+        private float _timeScale;
+        
+        [SerializeField]
+        private AnimationCurve timeCurve;
+
+        private float _timeCurveNormalization;
+
+        private void Awake()
+        {
+            NormalizeTimeCurve();
+        }
+
+        private void Update()
+        {
+            UpdateTimeScale();
+            UpdateTime();
+        }
+
+        private void UpdateTimeScale()
+        {
+            _timeScale = 24 / (dayLength / 60);
+            _timeScale *= timeCurve.Evaluate(timeOfDay);
+            _timeScale /= _timeCurveNormalization;
+        }
+
+        private void UpdateTime()
+        {
+            timeOfDay += Time.deltaTime * _timeScale / 86400; // current day time in seconds
+            if (timeOfDay >= 1)
+            {
+                timeOfDay = 0;
+                _days++;
+                if (_days >= yearLength)
+                {
+                    _days = 0;
+                    _years++;
+                }
+            }
+        }
+
+        private void NormalizeTimeCurve()
+        {
+            float stepSize = 0.01f;
+            int numberOfSteps = Mathf.FloorToInt(1f / stepSize);
+            float curveTotal = 0;
+
+            for (int i = 0; i < numberOfSteps; i++)
+            {
+                curveTotal += timeCurve.Evaluate(i * stepSize);
+            }
+
+            _timeCurveNormalization = curveTotal / numberOfSteps;
+        }
+    }
+}
---
+++ Assets/Scripts/GameTime/DayNightCycle.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using UnityEngine.Serialization;
+
+namespace GameTime
+{
+    public class DayNightCycle : MonoBehaviour
+    {
+        [SerializeField][Tooltip("Clock which is managing the game time ")]
+        private Clock clock;
+
+        [SerializeField] [Tooltip("Parent of the light sources")]
+        private Transform dayNightCycle;
+        
+        [SerializeField][Tooltip("Sun light source")]
+        private Light sunLight;
+        
+        [SerializeField][Tooltip("base intensity of the sun")]
+        private float sunBaseIntensity;
+
+        [SerializeField][Tooltip("A gradient that changes the sun color depending of day time")]
+        private Gradient sunColor;
+
+        [SerializeField][Tooltip("tilt of the sun")] [Range(-45f, 45f)]
+        private float sunTilt;
+        
+        
+        private float _intensity;
+
+        
+        private void Awake()
+        {
+        }
+
+        void Update()
+        {
+            RotateSun();
+            SunIntensity();
+            AdjustSunColor();
+        }
+        
+        private void RotateSun()
+        {
+            float sunAngle = clock.TimeOfDay * 360f;
+            dayNightCycle.transform.localRotation = Quaternion.Euler(new Vector3(sunTilt,0f, sunAngle));
+        }
+
+        private void SunIntensity()
+        {
+            _intensity = Mathf.Clamp01(Vector3.Dot(sunLight.transform.forward, Vector3.down));
+
+            sunLight.intensity = _intensity + sunBaseIntensity;
+        }
+
+        private void AdjustSunColor()
+        {
+            sunLight.color = sunColor.Evaluate(_intensity);
+        }
+        
+    }
+}
