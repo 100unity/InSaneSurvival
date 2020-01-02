f25b6a00d019cf75facf10c157cec98d8d7add73
Samuel Günther <samuell.guenther@gmail.com>
2020-01-02 16:11:20 +0100

    setup sanityspawner with sanityenemies

---
+++ Assets/Scripts/Spawner/SanitySpawner.cs
@@ -0,0 +1,27 @@
+﻿using Entity.Enemy;
+using UnityEngine;
+using UnityEngine.AI;
+using Utils;
+
+namespace Spawner
+{
+    public class SanitySpawner : NPCSpawner
+    {
+        protected override void TrySpawn()
+        {
+            
+            Vector3 spawnLocation = _navMeshMapper.GetMappedRandomPoint(spawnArea, NavMesh.AllAreas);
+            EnemyController spawned = Instantiate(toSpawn, spawnLocation, Quaternion.identity, transform);
+            Renderer renderer = spawned.Renderer;
+            renderer.enabled = true;
+            if (!renderer.InFrustum(mainCamera))
+                renderer.enabled = true;
+            else
+                Destroy(spawned.gameObject);
+            spawned.WanderArea.center = spawnArea.center;
+            
+            //base.TrySpawn();
+            //spawned.WanderArea.center = spawnArea.center;
+        }
+    }
+}
