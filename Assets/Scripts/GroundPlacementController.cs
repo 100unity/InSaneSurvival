61e4f2835078744ce7b3fb14917ed295acd49dac
Samuel Günther <samuell.guenther@gmail.com>
2020-01-03 19:07:55 +0100

    try automate ground placement

--- Assets/Scripts/GroundPlacementController.cs
+++ Assets/Scripts/GroundPlacementController.cs
@@ -1,18 +1,101 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
-public class GroundPlacementController : MonoBehaviour
+[InitializeOnLoad]
+public static class EditorHotkeysTracker
 {
-    // Start is called before the first frame update
-    void Start()
+    static EditorHotkeysTracker()
     {
-        
+        SceneView.duringSceneGui -= (view) => Method();
     }
 
-    // Update is called once per frame
-    void Update()
+    private static void Method()
     {
-        
+        var e = Event.current;
+        if (e != null && e.keyCode == KeyCode.A && e.type == EventType.KeyDown)
+        {
+            
+        }
+            
     }
+    /*
+    private static void HandleKeyDown()
+    {
+        if (_currentPlaceableObject == null)
+        {
+            _currentPlaceableObject = Instantiate(placeableObjectPrefab);
+        }
+        else
+        {
+            Destroy(_currentPlaceableObject);
+        }
+    }*/
 }
+/*
+public class GroundPlacementController : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject placeableObjectPrefab;
+
+    [SerializeField]
+    private KeyCode hotkey = KeyCode.A;
+
+    private GameObject _currentPlaceableObject;
+    private float _mouseWheelRotation;
+
+    private void Update()
+    {
+        HandleHotkey();
+
+        if (_currentPlaceableObject != null)
+        {
+            MoveCurrentPlaceableObjectToMouse();
+            RotateFromMouseWheel();
+            ReleaseIfClicked();
+        }
+    }
+
+    private void ReleaseIfClicked()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            _currentPlaceableObject = null;
+        }
+    }
+
+    private void RotateFromMouseWheel()
+    {
+        _mouseWheelRotation += Input.mouseScrollDelta.y;
+        _currentPlaceableObject.transform.Rotate(Vector3.up, _mouseWheelRotation * 10f);
+    }
+
+    private void MoveCurrentPlaceableObjectToMouse()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit))
+        {
+            _currentPlaceableObject.transform.position = hit.point;
+            _currentPlaceableObject.transform.rotation = Quaternion.FromToRotation(Vector3.up, hit.normal);
+        }
+    }
+
+    private void HandleHotkey()
+    {
+        if (Input.GetKeyDown(hotkey))
+        {
+            if (_currentPlaceableObject == null)
+            {
+                _currentPlaceableObject = Instantiate(placeableObjectPrefab);
+            }
+            else
+            {
+                Destroy(_currentPlaceableObject);
+            }
+        }
+    }
+}*/
