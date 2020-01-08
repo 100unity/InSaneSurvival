3979cf8257755aecde18c712aef68dad1938f84f
Tobi Hoffmann <tobi.hoffmann@protonmail.com>
2020-01-08 11:58:50 +0100

    sanity fog attributes are now controlled by curves

--- Assets/Scripts/Effects/FogOfSanity.cs
+++ Assets/Scripts/Effects/FogOfSanity.cs
@@ -5,9 +5,6 @@ namespace Effects
 {
     public class FogOfSanity : MonoBehaviour
     {
-        [SerializeField][Tooltip("Sanity level when effects begin to be visible")]
-        private float sanityImpactThreshhold;
-        
         [SerializeField][Tooltip("Reference to the fog generating mesh")]
         private Renderer fogOfSanityMesh;
     
@@ -20,25 +17,31 @@ namespace Effects
         [SerializeField] [Tooltip("Determines how fast pulse animation is")]
         private float pulseFrequency;
 
-        [SerializeField] [Tooltip("Determines the relation between sanity and fog radius")]
-        private float scaleFactor;    
+        [SerializeField][Tooltip("Determines the the center area size of the sanity texture")]
+        private AnimationCurve apertureRadiusCurve;
+        
+        [SerializeField][Tooltip("Determines the opacity of the center area size")]
+        private AnimationCurve apertureOpacityCurve;
+        
+        [SerializeField][Tooltip("Determines the opacity of the whole sanity mesh")]
+        private AnimationCurve materialOpacityCurve;
         
         // radius of the fog
-        private float _baseRadius;
-    
+        private float _apertureBaseRadius;
+
         // properties needed for the pulse animation
-        private float _currentRadius;
+        private float _apertureCurrentRadius;
         private bool _isGrowing;
    
         // shader properties
         private static readonly int PlayerPosition = Shader.PropertyToID("_Center");
-        private static readonly int FogRadius = Shader.PropertyToID("_FogRadius");
+        private static readonly int ApertureRadius = Shader.PropertyToID("_FogRadius");
         private static readonly int ApertureAlpha = Shader.PropertyToID("_ApertureAlpha");
 
         private void Awake()
         {
-            _baseRadius = 20;
-            _currentRadius = _baseRadius;
+            _apertureBaseRadius = 100;
+            _apertureCurrentRadius = _apertureBaseRadius;
             _isGrowing = true;
         }
     
@@ -62,17 +65,17 @@ namespace Effects
         /// <param name="intensity">Distance between min and max size of the radius</param>
         private void Pulse(float intensity)
         {
-            fogOfSanityMesh.material.SetFloat(FogRadius, _currentRadius);
+            fogOfSanityMesh.material.SetFloat(ApertureRadius, _apertureCurrentRadius);
         
             if (_isGrowing)
             {
-                _currentRadius += Time.deltaTime / pulseFrequency;
-                if (_currentRadius >= _baseRadius + intensity) _isGrowing = false;
+                _apertureCurrentRadius += Time.deltaTime / pulseFrequency;
+                if (_apertureCurrentRadius >= _apertureBaseRadius + intensity) _isGrowing = false;
             }
             else
             {
-                _currentRadius -= Time.deltaTime / (pulseFrequency / 2);
-                if (_currentRadius <= _baseRadius) _isGrowing = true;
+                _apertureCurrentRadius -= Time.deltaTime / (pulseFrequency / 2);
+                if (_apertureCurrentRadius <= _apertureBaseRadius) _isGrowing = true;
             }
         }
     
@@ -90,23 +93,17 @@ namespace Effects
 
         private void UpdateFogRadius(int sanityLevel)
         {
-            Color fogColor = fogOfSanityMesh.material.color;
-            fogColor.a = Mathf.Lerp(0.2f, 1f, sanityLevel/100f);
-            fogOfSanityMesh.material.color = fogColor;
-                
-            float apertureAlpha = Mathf.Lerp(1, 10, sanityLevel/200f);
-            fogOfSanityMesh.material.SetFloat(ApertureAlpha, apertureAlpha);
+            float sanity = (float) sanityLevel / 100;
             
-            if (sanityLevel > sanityImpactThreshhold)
-            {
-                _baseRadius = 100;
-            }
-            else
-            {
-                
-                
-                _baseRadius = sanityLevel * scaleFactor;
-            }
+            _apertureBaseRadius = apertureRadiusCurve.Evaluate(sanity) * 100;
+            
+            float apertureOpacity = apertureOpacityCurve.Evaluate(sanity);
+            fogOfSanityMesh.material.SetFloat(ApertureAlpha, Mathf.Lerp(1, 10, apertureOpacity));
+            
+            float materialOpacity = materialOpacityCurve.Evaluate(sanity);
+            Color fogColor = fogOfSanityMesh.material.color;
+            fogColor.a = Mathf.Lerp(0.2f, 1f, materialOpacity);
+            fogOfSanityMesh.material.color = fogColor;
         }
     }
 }
