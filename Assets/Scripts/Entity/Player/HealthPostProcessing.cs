1273239260627a87c3042210bc5fc39a38564651
Samuel Günther <samuell.guenther@gmail.com>
2020-01-15 18:17:46 +0100

    finishing red screen, refactoring, tweaking

--- Assets/Scripts/Entity/Player/HealthPostProcessing.cs
+++ Assets/Scripts/Entity/Player/HealthPostProcessing.cs
@@ -1,98 +1,82 @@
-﻿using Entity.Player;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.Rendering.PostProcessing;
+using Utils;
 
-public class HealthPostProcessing : MonoBehaviour
+namespace Entity.Player
 {
-    [Tooltip("The intensity curve that scales the red screen effect depending on the player's health.")]
-    [SerializeField]
-    private AnimationCurve intensityCurve;
-
-    [Tooltip("Frequency of vignette pulsing.")]
-    [SerializeField]
-    private float vignettePulseFrequency;
-
-    [Tooltip("Intensity of the vignette pulse.")]
-    [SerializeField]
-    [Range(0, 1)]
-    private float vignettePulseIntensity;
-
-    [Tooltip("The minimum sanity the player can have in order to display the red screen. If sanity goes below, red screen is not shown, but sanity vignette takes over.")]
-    [SerializeField]
-    [Range(0, 100)]
-    private float sanityCap;
-    
-    private int _health;
-    private int _sanity;
-    // have different frequencies for different effects
-    private bool _isGrowing;
-
-    private PostProcessVolume _postProcessVolume;
-
-    private Vignette _vignette;
-    private float _vignetteBaseIntensity;
-    private float _currentVignetteIntensity;
-    private float _vignettePulse;
-
-    private void Awake()
+    public class HealthPostProcessing : MonoBehaviour
     {
-        _postProcessVolume = GetComponent<PostProcessVolume>();
-        _postProcessVolume.profile.TryGetSettings(out _vignette);
-        _vignetteBaseIntensity = _vignette.intensity.value;
-        if (vignettePulseIntensity + _vignetteBaseIntensity > 1)
-            vignettePulseIntensity = 1 - _vignetteBaseIntensity;
-        _health = 100;
-    }
+        [Tooltip("The intensity curve that scales the red screen effect depending on the player's health.")]
+        [SerializeField]
+        private AnimationCurve intensityCurve;
 
-    private void OnEnable()
-    {
-        PlayerState.OnPlayerHealthUpdate += OnHealthUpdated;
-        PlayerState.OnPlayerSanityUpdate += OnSanityUpdated;
-    }
+        [Tooltip("Maximum frequency of vignette pulsing.")]
+        [SerializeField]
+        private float maxVignettePulseFrequency;
 
-    private void OnDisable()
-    {
-        PlayerState.OnPlayerHealthUpdate -= OnHealthUpdated;
-        PlayerState.OnPlayerSanityUpdate += OnSanityUpdated;
-    }
+        [Tooltip("Intensity of the vignette pulse.")]
+        [SerializeField]
+        [Range(0, 1)]
+        private float vignettePulseIntensity;
 
-    private void Update()
-    {
-        if (_sanity < sanityCap)
-            return;
+        [Tooltip("The minimum sanity the player can have in order to display the red screen. If sanity goes below, red screen is not shown, but sanity vignette takes over.")]
+        [SerializeField]
+        [Range(0, 100)]
+        private float sanityCap;
 
-        _vignettePulse = Pulse(_vignettePulse, vignettePulseFrequency, vignettePulseIntensity, ref _isGrowing);
-        float y = intensityCurve.Evaluate(_health / 100f);
-        _currentVignetteIntensity = _vignetteBaseIntensity + y * _vignettePulse;
-        Debug.Log(_health);
-        _vignette.intensity.value = _currentVignetteIntensity;
-    }
+        private Pulser _pulser;
+        private int _health;
+        private int _sanity;
 
-    private void OnHealthUpdated(int health) => _health = health;
-    private void OnSanityUpdated(int sanity) => _sanity = sanity;
+        private PostProcessVolume _postProcessVolume;
+        private Vignette _vignette;
+        private float _vignetteBaseIntensity;
+        private float _vignettePulse;
 
-    /// <summary>
-    /// Pulse a value with a certain frequency and intensity.
-    /// </summary>
-    /// <param name="valueToBePulsed">The value pulsing should be applied to.</param>
-    /// <param name="pulseFrequency">The frequency of the pulse.</param>
-    /// <param name="pulseIntensity">The intensity of the pulse.</param>
-    /// <param name="boolIndex">Index of the boolean to be used for creating the pulse effect.</param>
-    /// <returns></returns>
-    private float Pulse(float valueToBePulsed, float pulseFrequency, float pulseIntensity, ref bool isGrowing)
-    {
-        if (isGrowing)
+        private void Awake()
         {
-            valueToBePulsed += Time.deltaTime * pulseFrequency;
-            if (valueToBePulsed > pulseIntensity)
-                isGrowing = false;
+            _pulser = new Pulser();
+            _postProcessVolume = GetComponent<PostProcessVolume>();
+            _postProcessVolume.profile.TryGetSettings(out _vignette);
+            // get base value
+            _vignetteBaseIntensity = _vignette.intensity.value;
+            // clamp intensity
+            if (vignettePulseIntensity + _vignetteBaseIntensity > 1)
+                vignettePulseIntensity = 1 - _vignetteBaseIntensity;
+            _health = 100;
+            _sanity = 100;
         }
-        else
+
+        private void OnEnable()
         {
-            valueToBePulsed -= Time.deltaTime * (pulseFrequency / 2);
-            if (valueToBePulsed <= 0)
-                isGrowing = true;
+            PlayerState.OnPlayerHealthUpdate += OnHealthUpdated;
+            PlayerState.OnPlayerSanityUpdate += OnSanityUpdated;
         }
-        return valueToBePulsed;
+
+        private void OnDisable()
+        {
+            PlayerState.OnPlayerHealthUpdate -= OnHealthUpdated;
+            PlayerState.OnPlayerSanityUpdate += OnSanityUpdated;
+        }
+
+        /// <summary>
+        /// Applies red screen effect with varying frequency and intensity depending on the player's health if sanity is high enough.
+        /// </summary>
+        private void Update()
+        {
+            if (_sanity < sanityCap)
+                return;
+
+            float y = intensityCurve.Evaluate(_health / 100f);
+            float currentVignettePulseFrequency = maxVignettePulseFrequency * y;
+
+            // pulse
+            _vignettePulse = _pulser.Pulse("vignette", _vignettePulse, currentVignettePulseFrequency, vignettePulseIntensity);
+            // update with masked value
+            _vignette.intensity.value = _vignetteBaseIntensity + y * _vignettePulse;
+        }
+
+        private void OnHealthUpdated(int health) => _health = health;
+        private void OnSanityUpdated(int sanity) => _sanity = sanity;
     }
 }
