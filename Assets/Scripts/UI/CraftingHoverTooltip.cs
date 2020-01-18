b3faf0d41af4dc254869c67920df81fcfbe1e001
David <panagiotopulos.d@gmail.com>
2020-01-18 17:03:26 +0100

    [Added some summaries] [Added crafting station to tooltip]

--- Assets/Scripts/UI/CraftingHoverTooltip.cs
+++ Assets/Scripts/UI/CraftingHoverTooltip.cs
@@ -2,33 +2,51 @@
 using Managers;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 using Utils;
 
 namespace UI
 {
     public class CraftingHoverTooltip : HoverTooltip
     {
-        [Header("CraftingTooltip")] [Tooltip("")] [SerializeField]
+        [Header("CraftingTooltip")]
+        [Tooltip("The CraftingRecipeUI reference used for getting the recipe")]
+        [SerializeField]
         private CraftingRecipeUI craftingRecipe;
 
-        [Tooltip("")] [SerializeField] private TextMeshProUGUI txtTitle;
+        [Tooltip("Title-Text used for setting it to the recipes name")] [SerializeField]
+        private TextMeshProUGUI txtTitle;
 
-        [Tooltip("")] [SerializeField] private Transform resourceParent;
+        [Tooltip("CarftingStation-Text used for displaying the needed crafting station")] [SerializeField]
+        private TextMeshProUGUI txtCraftingStation;
+
+        [Tooltip("The layoutGroup used for setting the correct parent")] [SerializeField]
+        private LayoutGroup resourceParent;
 
         [SerializeField] private CraftingHoverTooltipResource craftingHoverTooltipResourcePrefab;
 
-
+        /// <summary>
+        /// Waits for the recipe to be loaded, then invokes <see cref="Setup"/>.
+        /// </summary>
         protected override void Awake()
         {
             base.Awake();
             CoroutineManager.Instance.WaitUntil(() => craftingRecipe.Recipe != null, Setup);
         }
 
+        /// <summary>
+        /// Sets the texts and images.
+        /// </summary>
         private void Setup()
         {
             txtTitle.SetText(craftingRecipe.Recipe.CreatedItemName);
+            if (craftingRecipe.Recipe.CraftingStation == CraftingManager.CraftingStation.None)
+                txtCraftingStation.gameObject.SetActive(false);
+            else
+                txtCraftingStation.SetText(
+                    $"Crafting Station: <color=red>{craftingRecipe.Recipe.CraftingStation.ToString()}</color>");
             foreach (ItemResourceData resourceData in craftingRecipe.Recipe.NeededItems)
-                Instantiate(craftingHoverTooltipResourcePrefab, resourceParent).Init(resourceData);
+                Instantiate(craftingHoverTooltipResourcePrefab, resourceParent.transform).Init(resourceData);
         }
     }
 }
