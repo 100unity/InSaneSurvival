6ed8c3eb61b59efa732a8a50335f433a03b348fe
Tobi Hoffmann <tobi.hoffmann@protonmail.com>
2020-01-27 19:06:02 +0100

    can now load saves from main menu

--- Assets/Scripts/UI/Menus/MainMenu.cs
+++ Assets/Scripts/UI/Menus/MainMenu.cs
@@ -1,4 +1,5 @@
-﻿using Constants;
+﻿using System;
+using Constants;
 using Managers;
 using UnityEngine;
 using UnityEngine.SceneManagement;
@@ -8,7 +9,13 @@ namespace UI.Menus
 {
     public class MainMenu : MonoBehaviour
     {
-        [Tooltip("Button for starting the game")] [SerializeField]
+        [Tooltip("Button for continuing an saved game")] [SerializeField]
+        private GameObject continueButton;
+        
+        [Tooltip("Default filename of the savegame to load with continue button")] [SerializeField]
+        private string saveGameFile;
+        
+        [Tooltip("Button for starting a new game")] [SerializeField]
         private Button playButton;
 
         [Tooltip("Button for opening the settings")] [SerializeField]
@@ -25,20 +32,42 @@ namespace UI.Menus
 
         [Tooltip("The content of the options menu")] [SerializeField]
         private GameObject optionsContent;
-
+        
         /// <summary>
         /// Adds all listeners
         /// </summary>
         private void Awake()
         {
+            CheckForSaveGame();
             playButton.onClick.AddListener(Play);
             optionsButton.onClick.AddListener(() => Options(true));
             optionsBackButton.onClick.AddListener(() => Options(false));
             quitButton.onClick.AddListener(GameManager.Quit);
+            
+        }
+
+        private void CheckForSaveGame()
+        {
+            string pathToFile = @"" + Application.persistentDataPath + "/" + saveGameFile + ".json";
+             
+            if(System.IO.File.Exists(pathToFile))
+            {
+                continueButton.GetComponent<Button>().onClick.AddListener(Continue);
+                continueButton.SetActive(true);
+            }
         }
 
         /// <summary>
-        /// Loads the GameScene
+        /// Loads the GameScene in a previously saved state
+        /// </summary>
+        private void Continue()
+        {
+            SceneManager.LoadScene(Consts.Scene.GAME);
+            SceneManager.sceneLoaded += OnSceneLoaded;
+        }
+
+        /// <summary>
+        /// Loads the GameScene as a new game
         /// </summary>
         private void Play() => SceneManager.LoadScene(Consts.Scene.GAME);
 
@@ -50,6 +79,15 @@ namespace UI.Menus
         {
             mainMenuContent.SetActive(!show);
             optionsContent.SetActive(show);
+        }
+        
+        /// <summary>
+        /// Waits for GameScene to be loaded, then loads Save
+        /// </summary>
+        private void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+        {
+            SaveManager.Instance.Load(saveGameFile);
+            SceneManager.sceneLoaded -= OnSceneLoaded;
         }
     }
 }
