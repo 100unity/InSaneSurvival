6d960980fc2a71e1b03788621bbfceffe202060d
Tobi Hoffmann <tobi.hoffmann@protonmail.com>
2020-01-30 22:38:14 +0100

    can cheat wool now

--- Assets/Scripts/Remote/ItemCheater.cs
+++ Assets/Scripts/Remote/ItemCheater.cs
@@ -9,6 +9,7 @@ namespace Remote
         [SerializeField] private Item wood;
         [SerializeField] private Item stone;
         [SerializeField] private Item iron;
+        [SerializeField] private Item wool;
 
         private InventoryManager _inventoryManager;
 
@@ -30,6 +31,31 @@ namespace Remote
                 case "IRON":
                     AddIron();
                     break;
+                case "WOOL":
+                    AddWool();
+                    break;
+                case "LOOTGOBLIN":
+                    AddEverything();
+                    break;
+            }
+        }
+
+        private void AddEverything()
+        {
+            for (int i = 0; i < 20; i++)
+            {
+                _inventoryManager.AddItem(wool);
+                _inventoryManager.AddItem(iron);
+                _inventoryManager.AddItem(wood);
+                _inventoryManager.AddItem(stone);
+            }
+        }
+        
+        private void AddWool()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                _inventoryManager.AddItem(wool);
             }
         }
 
