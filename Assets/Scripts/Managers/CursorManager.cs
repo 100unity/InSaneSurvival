2bed9137fa673a4e90d1fc4f1fa6ca1cc7e93bed
David <panagiotopulos.d@gmail.com>
2020-01-30 21:03:01 +0100

    [Added new Interact tooltip for better performance]

---
+++ Assets/Scripts/Managers/CursorManager.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace Managers
+{
+    public class CursorManager : Singleton<CursorManager>
+    {
+        private readonly Vector2 _zeroVector2 = Vector2.zero;
+
+        public void SetCursorIcon(Texture2D newCursorIcon)
+        {
+            Vector2 hotspot = new Vector2(newCursorIcon.width / 2f, newCursorIcon.height / 2f);
+            Cursor.SetCursor(newCursorIcon, hotspot, CursorMode.Auto);
+        }
+
+        public void SetCursorToDefault() => Cursor.SetCursor(null, _zeroVector2, CursorMode.Auto);
+    }
+}
